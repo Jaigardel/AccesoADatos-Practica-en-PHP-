c635ff71c6881be76da63c2ee371fa2585e5aadd
Jaime Garcia de la Concha <jaigardel144@gmail.com>
2025-01-31 12:29:49 +0100

    Update variables.php

--- Empresa con registro y roles/utiles/variables.php
+++ Empresa con registro y roles/utiles/variables.php
@@ -1,11 +1,8 @@
 <?php
-	/*IMPORTANTE!! para que mi aplicación funcione la base de datos TIENE que permitir que en la tabla de USUARIOS, 
-	los campos para PASSWORD y TOKEN puedan tomar valores NULL!!*/
-
 	
 	// Definición de variables
-	$host = "192.168.1.136";
-	$user = "pepito01";
-	$password = "pepito01";
+	$host = "";
+	$user = "";
+	$password = "";
 	$bbdd = "Aplicacion_Empresa";
 ?>
